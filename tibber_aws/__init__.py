eff11029295aff7e1336c64a7a6f43bda8923aa6
Daniel Hjelseth Høyer <mail@dahoiv.net>
2021-06-07 13:53:07 +0200

    aws_lambda

--- tibber_aws/__init__.py
+++ tibber_aws/__init__.py
@@ -1,6 +1,6 @@
 # flake8: noqa
 from .aws_queue import Queue
-from .lambda import invoke as lambda_invoke
+from .aws_lambda import invoke as lambda_invoke
 from .s3 import S3Bucket
 from .secret_manager import get_secret, get_secret_parser
 from .sns import Topic
