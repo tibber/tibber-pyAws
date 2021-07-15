3b9f1b9b7fd4949c7a63ac494cd6cbd3fea7a395
Daniel Hjelseth Høyer <mail@dahoiv.net>
2021-07-15 13:01:33 +0200

    0.7.1

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ setup(
     name="tibber_aws",
     packages=["tibber_aws"],
     install_requires=install_requires,
-    version="0.7.0",
+    version="0.7.1",
     description="A python3 library to communicate with Aws",
     python_requires=">=3.5.3",
     author="Tibber",
