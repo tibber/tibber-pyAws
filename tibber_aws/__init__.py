4b8a9c80b7f032bb13ca73dc7d9e35ba7039c91d
Daniel Hjelseth Høyer <mail@dahoiv.net>
2021-08-27 09:39:25 +0200

    Update __init__.py

--- tibber_aws/__init__.py
+++ tibber_aws/__init__.py
@@ -1,6 +1,7 @@
 # flake8: noqa
 from .aws_lambda import invoke as lambda_invoke
 from .aws_queue import Queue
+from .aws_metadata import get_instance_id
 from .s3 import STATE_NOT_EXISTING, STATE_OK, STATE_PRECONDITION_FAILED, S3Bucket
 from .secret_manager import get_secret, get_secret_parser
 from .sns import Topic
