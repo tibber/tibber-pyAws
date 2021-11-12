f3cf9d9656dd0ecc8bb3e8676682a63678ee7425
Victor <victor@tibber.com>
2021-11-12 11:31:42 +0100

    Add await to delete_call, fix function call name

--- tibber_aws/s3.py
+++ tibber_aws/s3.py
@@ -50,8 +50,8 @@ class S3Bucket(AwsBase):
         return data, state
 
     async def delete_file(self, key):
-        await self.init_client_if_required()
-        self._client.delete_object(Bucket=self._bucket_name, Key=key)
+        await self._init_client_if_required()
+        await self._client.delete_object(Bucket=self._bucket_name, Key=key)
 
     async def store_data(self, key, data, retry=1):
         await self.init_client_if_required()
