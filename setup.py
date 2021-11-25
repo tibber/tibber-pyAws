16be39558595e6eb49306aedbeb46d5eb0638204
Victor <victor@tibber.com>
2021-11-25 16:07:53 +0100

    update version in setup.py to 0.9.4

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ setup(
     name="tibber_aws",
     packages=["tibber_aws"],
     install_requires=install_requires,
-    version="0.9.3",
+    version="0.9.4",
     description="A python3 library to communicate with Aws",
     python_requires=">=3.5.3",
     author="Tibber",
