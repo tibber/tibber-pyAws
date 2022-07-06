abb6116b417985e21c7275ee126cade376c7c568
Caspar Nettelbladt <caspar.n@gmail.com>
2022-07-07 00:50:20 +0200

    Update example

--- examples/aws_queue_listener.py
+++ examples/aws_queue_listener.py
@@ -11,10 +11,22 @@ async def handle_test_message(msg: SqsMessage):
     print(msg.message)
 
 
+def create_message_handler() -> Callable[[SqsMessage], Awaitable]:
+    """Create message handler."""
+
+    async def message_handler(message: SqsMessage):
+        if message.subject == "YOUR SUBJECT":
+            return await handle_test_message(message)
+        return True
+
+    return message_handler
+
+
 async def main():
+
     listener = SqsListener(
         queue_url="test-py-aws-queue",
-        message_handler=handle_test_message,
+        message_handler=create_message_handler(),
     )
 
     await listener.run()
